055672d77514c8f3c2187682065ee2d6c4a5f980
Simon <skidrevenant@gmail.com>
2018-03-03 21:53:32 +0100

    started on the project

---
+++ src/main/java/party/cuckcatcher/impl/CuckCatcher.java
@@ -0,0 +1,17 @@
+package party.cuckcatcher.impl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Made by SkidRevenant at 03/03/2018
+ */
+public class CuckCatcher extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
